3349de46e205a004627c5fb34c04180ae389512f
root <dr.dangers@gmail.com>
2017-01-20 19:44:09 -0800

    -Track users name/color/ID -Active users displayed in right panel -Join and Leave messages in chat -Custom Names / Colors per user

--- index.js
+++ index.js
@@ -4,18 +4,56 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', { root: __dirname });
+	res.sendFile('index.html', { root: __dirname });
 });
 
+
+var clients = [];
 io.on('connection', function(socket){
-  socket.on('chat message', function(name, msg){
-    io.emit('chat message', name, msg);
-  });
+
+	//add a client to our array with empty name and color fields
+	clients.push({id: socket.id, name: 'tmp', color: '#000'});
+	//push the id to the client, so they can respond with their color and name
+	io.sockets.connected[socket.id].emit('set id', socket.id);
+	//client response - set their color and name, and then emit to all clients to update user list
+	socket.on('connection message', function(msg){
+		var index = searchId(socket.id)
+		console.log(index);
+		if (index > -1) {
+			clients[index].name = msg.name;
+			clients[index].color = msg.color;
+    		io.emit('connection message', msg, clients);
+    	}
+	});
+
+	//simple emit to all clients
+	socket.on('chat message', function(name, msg){
+    	io.emit('chat message', name, msg);
+	});
+
+	//on disconnect remove them from our array of clients and have all clients rebuild their user list
+	socket.on('disconnect', function(){
+		var index = searchId(socket.id);
+		if (index > -1) {
+			var dc = {name: clients[index].name, color: clients[index].color};
+			clients.splice(index, 1);
+			console.log(socket.id + ' disconnected');
+			io.emit('disconnect message', dc, clients);
+		}
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
+
+function searchId(sid){
+	for (var i=0; i<clients.length; i++){
+		if (clients[i].id === sid)
+			return i;
+	}
+	return -1;
+}
 
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/styles", express.static(__dirname + '/styles'));
--- js/chat.js
+++ js/chat.js
@@ -1,11 +1,54 @@
 var socket = io();
-var color = '#' + (function co(lor){   return (lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && (lor.length == 6) ?  lor : co(lor); })('');
-
+var myColor = ['#158178','#6036da','#6e368f','#f73c59','#6a6832','#511583','#436b34','#2e0106','#596466','#f7492b','#468db0','#c15a1f','#594b04','#4a739a','#41682a','#0d4c30'][Math.floor(Math.random()*16)];
 var sendButton = $('#sendButton');
+var msgBox = $('#messageBox');
+var users = $('#users');
+var nameField = $('#name').val(['Bobbert','Tommert','Ralphert','Larimous','Dannert','Babush','The Catfish is the King of the Pontar','Sammert','Frankert','Robbert','Smithert','Seannert','Kevert','Jeremert','Mikert','Jonert'][Math.floor(Math.random()*16)]);
+var myId;
+
+
+socket.on('set id', function(id){
+	myId = id;
+	console.log(myId);
+	socket.emit('connection message', 	{ 	color:myColor, 
+											name:nameField.val(), 
+											msg:' has connected to the server!', 
+											id: myId });
+});
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+socket.on('connection message', function(msg, clients){
+	rebuildUserList(clients);
+	var item = $('<li/>').addClass('connection');
+	var uname = $('<p/>').addClass('name').text(msg.name).appendTo(item).css("color", msg.color);
+	var message = $('<p/>').addClass('connectiontext').text(msg.msg).appendTo(item);
+	$('#messages').append(item);
+	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+});
+
+socket.on('disconnect message', function(dc, clients){
+	rebuildUserList(clients);
+	var item = $('<li/>').addClass('disconnect');
+	var uname = $('<p/>').addClass('name').text(dc.name).appendTo(item).css("color", dc.color);
+	var message = $('<p/>').addClass('disconnecttext').text(' has disconnected!').appendTo(item);
+	$('#messages').append(item);
+	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+});
+
+function rebuildUserList(clients){
+	users.empty();
+	for (var i in clients){
+		console.log(clients[i].name);
+		users.append($('<p/>').text(clients[i].name));
+	}
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 sendButton.click(function(){
 	sendMessage();
 });
-var msgBox = $('#messageBox');
+
 msgBox.keydown(function(e){
 	if(e.keyCode == 13) {
 		sendMessage();
@@ -14,7 +57,7 @@ msgBox.keydown(function(e){
 
 function sendMessage() {
 	var msg = msgBox.val();
-	var name = $('#name').val();
+	var name = {name:$('#name').val(), color:myColor};
 	if (msg !== '') {
 		socket.emit('chat message', name, msg);
 		msgBox.val('');
@@ -23,9 +66,14 @@ function sendMessage() {
 
 socket.on('chat message', function(name, msg){
 	var item = $('<li/>');
-	var uname = $('<p/>').addClass('name').text(name+':').appendTo(item).css("color", color);
-	var message = $('<p/>').addClass('text').text(msg).appendTo(item);
+	var message = $('<p/>').addClass('text').appendTo(item);
+	var uname = $('<span/>').addClass('name').text(name.name+':').appendTo(message).css("color", name.color);
+	message.append(msg);
 
 	$('#messages').append(item);
 	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
 });
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
